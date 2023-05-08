25aae5743eebdb0b28216d7064d90f3d4e09ab0b
Pete Wond <pete@alberon.co.uk>
2023-05-08 22:18:24 +0100

    Play with flash messages and dialogs for updates and deletes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 # setup
 app = Flask(__name__)
+app.secret_key = b'somethingsomethingdarkside' # need a session to use flashes and session needs a secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
@@ -21,52 +22,57 @@ class Todo(db.Model):
 with app.app_context():
         db.create_all()
 
-# route handler with decorator route binding
-@app.route('/', methods=['GET', 'POST'])
+# route handlers with decorator route binding
+@app.get('/')
 def index():
-	if request.method == 'POST':
-		task_content = request.form['content']
+	tasks = Todo.query.order_by(Todo.date_created).all()
+	return render_template('index.html', tasks=tasks)
+	
+@app.post('/')
+def create():
+	task_content = request.form['content']
 
-		if task_content == '':
-			return redirect('/')
+	if task_content == '':
+		return redirect('/')
 
-		new_task = Todo(content=task_content)
-		
-		try:
-			db.session.add(new_task)
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'Its not good news'
-	else:
-		tasks = Todo.query.order_by(Todo.date_created).all()
-		return render_template('index.html', tasks=tasks)
+	new_task = Todo(content=task_content)
+	
+	try:
+		db.session.add(new_task)
+		db.session.commit()
+		flash('New task added successfully', 'success')
+	except:
+		flash('There was a problem creating this task', 'error')
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
+	return redirect('/')	
+	
+# adding post and put to update route to handle both http and html form submissions
+@app.route('/update/<int:id>', methods=["POST", "PUT"])
 def update(id):
 	task_to_update = Todo.query.get_or_404(id)
-	
-	if request.method == 'POST':
-		task_to_update.content = request.form['content']
+	task_to_update.content = request.form['content']
 
-		try:
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'Its not good news'
-	else:
-		return render_template('update.html', task=task_to_update)
+	try:
+		db.session.commit()
+		flash('Task successfully updated', 'success')
+	except:
+		flash('There was a problem updating this task.', 'error')
 
-@app.route('/delete/<int:id>')
+	return redirect('/')
+
+# adding post and delete to update route to handle both http and html form submissions	
+@app.route('/delete/<int:id>', methods=["POST", "DELETE"])
 def delete(id):
 	task_to_delete = Todo.query.get_or_404(id)
 	
 	try:
 		db.session.delete(task_to_delete)
 		db.session.commit()
-		return redirect('/')
+		flash('Task successfully deleted', 'success')
 	except:
-		return 'Its not good news'
+		flash('There was a problem deleting this task.', 'error')
+
+	return redirect('/')
 
 # run script in debug mode for dev
 if __name__ == "__main__":
